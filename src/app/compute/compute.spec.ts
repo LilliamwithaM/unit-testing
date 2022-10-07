83d084007486dbb1034244f81a767b97c3845919
Patidifusa <zs20006765@estudiantes.uv.mx>
2022-10-07 02:47:09 -0500

    update issue compute #1

--- src/app/compute/compute.spec.ts
+++ src/app/compute/compute.spec.ts
@@ -1,13 +1,14 @@
 import { compute } from './compute';
 
-describe('compute', () => {
-    it('should return 0 if input is negative', () => {
-        const result = compute(-1);
-        expect(result).toBe(0);
-    })
+describe('compute test suite', ()=> {
 
-    it('should increment the input if it is positive', () => {
+	it ('should return 0 if input es negative', () => {
+        const result = compute(-1);
+	    expect(result).toBe(0);
+	})
+
+	it ('should increment the input if its positive', () => {
         const result = compute(1);
-        expect(result).toBe(2);
-    })
+	    expect(result).toBe(2);
+	})
 })
