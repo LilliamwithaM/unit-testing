d0212ff66628260119d4f50e0f01a56c8c53b9bd
Patidifusa <zs20006765@estudiantes.uv.mx>
2022-10-10 09:59:30 -0500

    update pow #2

---
+++ src/app/pow/pow.ts
@@ -0,0 +1,16 @@
+export function pow(x: number, n:number){
+    var r =1;
+    if(n<0){
+        r=0;
+    }
+    if(n==0){
+        r=1;
+    }
+    else{
+        for(var i=1; i<=n; i++){
+            r=r*x;
+        }
+    }
+    return r;
+}
+
