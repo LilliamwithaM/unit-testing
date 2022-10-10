b9cfaaebd20bc545c15a32f09d16f295325f87c6
Patidifusa <zs20006765@estudiantes.uv.mx>
2022-10-10 09:24:14 -0500

    update #5

--- src/app/greet/greet.spec.ts
+++
@@ -1,7 +0,0 @@
-import { greet } from "./greet";
-
-describe('greet', () => {
-    it("'should include the name in message'", () => {
-        expect(greet('mosh')).toBe('Welcome mosh');
-    });
-});
